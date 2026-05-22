524cd3ab1b4360778d91adf908710110db3cb2f3
qin309 <392339828@qq.com>
2026-05-22 09:09:39 +0800

    fix: 添加缺失的主启动类VaccinationSystemApplication

---
+++ vaccination-system-backend/src/main/java/com/vaccination/VaccinationSystemApplication.java
@@ -0,0 +1,11 @@
+package com.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationSystemApplication.class, args);
+    }
+}
